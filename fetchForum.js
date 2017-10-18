1a4568a26308328565e9400fa403957b52b6b003
Helmi Fakhriandi Rachman <92hell@gmail.com>
2017-10-18 16:26:57 +0700

    update fetchForum.js to match main file

--- fetchForum.js
+++ fetchForum.js
@@ -1,15 +1,39 @@
 const request = require("request");
-const postNumber = 4610;
+const config = require("./config.json");
+const fs = require('fs');
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const postNumber = config.postNumber;
 const url = "https://www.plug.game/kingsraid-en/posts/"+postNumber;
 
-request({
-    url: url,
-    json: true
-}, function (error, response, body) {
+	request({
+    	url: url,
+    	json: true
+		}, function (error, response, body) {
+    		if (!error && response.statusCode === 200) {
+    			let newPostNumber = postNumber+1;
+    			config.postNumber = newPostNumber;
+    			fs.writeFile("./config.json", JSON.stringify(config), (err) => console.error);
 
-    if (!error && response.statusCode === 200) {
-    	var regex = new RegExp('<h3 class="board_name">(.*?)<\/h3>');
-        var tag = body.match(regex);
-        console.log(tag[1]) // Print the json response
-    }
-});
+    			var regex = new RegExp('<h3 class="board_name">(.*?)<\/h3>');
+        		var tag = body.match(regex);
+        		console.log('Post Number '+postNumber+' Tagged '+tag[1]); // Print the json response
+
+        		var postType = tag[1];
+
+        		const guild = client.guilds.find('name', 'ProjectXV');
+        		if(postType === 'Notices'){
+        			guild.channels.find('name', 'news').send(url);
+        		}
+        		if(postType === 'Patch Note'){
+        			guild.channels.find('name', 'news').send(url);
+        		}
+        		if(postType === 'Events'){
+        			guild.channels.find('name', 'events').send(url);
+        		}
+    		}
+    		else{
+    			console.log('no new post');
+    		}
+		}
+	);
